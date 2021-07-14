7878758afeaa0796d15a3abcf8317f44d48af613
riz-mbn <rizlee@mybizniche.com>
2021-07-14 18:54:34 +0800

    page header

--- templates/page-head.php
+++ templates/page-head.php
@@ -9,7 +9,7 @@ $add_buttons = get_field('has_call_to_action');
 
 ?>
 
-<?php if( !is_front_page() && is_home()) : ?>
+<?php if( !is_home()) : ?>
 
 <?php if( $banner_height == "full_height" ) : ?>
 <section class="hero_banner_wrap <?php echo $banner_height; ?>" data-body-class="<?php echo $banner_height; ?>" data-height="110vh">
@@ -98,7 +98,7 @@ $add_buttons = get_field('has_call_to_action');
         </div>
 	</div>
 </section>
-<?php else : ?>
+<?php elseif( is_home() ) : ?>
     <section class="hero hero-s4">
         <!-- Hero Desktop -->
         <div class="hero-block hero-block_desktop">
@@ -123,8 +123,7 @@ $add_buttons = get_field('has_call_to_action');
 								?>
                             </div>         
                         </div>
-                        <div class="divider_double_line"></div>  
-                                              
+                        <div class="divider_double_line"></div>                                                
                         <?php 
 						$show_checklists = get_field('show_checklists');
 						
@@ -160,78 +159,33 @@ $add_buttons = get_field('has_call_to_action');
                 <div class="grid-container"> 
                     <div class="grid-x hero-caption">
                         <div class="cell small-10 small-offset-1 large-5 large-offset-0 align-self-bottom">
-						<?php if ($add_image): ?><figure class="right_img text-center"><img src="<?php echo esc_attr($add_image);?>"></figure> <?php endif; ?>            
-                            <div class="text_wrap text-center right_text">
-                                <h2 class="ct_margin_both_0">Carol Royse</h2>
-                                <p class="small">REAL ESTATE EXPERT & RADIO HOST <Br/>OF THE CAROL ROYSE REAL ESTATE RADIO SHOW</p>
-                            </div>                      
-                            <div class="text_wrap row_column_flex text-center">
-                                <div class="row_col row_col_3">
-                                    <h2 class="ct_margin_both_0">Vikki</h2>
-                                    <h3>Royse-Middlebrook</h3>
-                                </div>
-                                <div class="row_col_3 row_column_flex">                                
-                                    <div class="row_col_2">
-                                        <figure class="text-center"><img src="<?php echo MBN_ASSETS_URI ?>/img/logo-ktar.png" alt=""></figure>
-                                        <span>
-                                            <p class="ct_twoline_text tiny">Saturday</p>
-                                            <p class="ct_twoline_text tiny">2:00 to 3:00pm</p>
-                                        </span>
-                                    </div>                                
-                                    <div class="row_col row_col_2">
-                                        <figure class="text-center"><img src="<?php echo MBN_ASSETS_URI ?>/img/logo-550kfyi.png" alt=""></figure>
-                                        <span>
-                                            <p class="ct_twoline_text tiny">Sunday</p>
-                                            <p class="ct_twoline_text tiny">2:00 to 3:00pm</p>
-                                        </span>
-                                    </div>
-                                </div>
-                                <div class="row_col row_col_3">
-                                    <h2 class="ct_margin_both_0 ct_twoline_text">Tim</h2>
-                                    <h2 class="ct_margin_both_0 ct_twoline_text">Evans</h2>
-                                </div>
-                            </div>
+						<?php if ($add_image): ?><figure class="right_img text-center"><img src="<?php echo esc_attr($add_image);?>"></figure> <?php endif; ?>    
                         </div>
                     </div>
                 </div> 
             </div>        
             <div class="hero-block hero_mobile_bottom">
                 <div class="grid-container">
-                    <div class="sec_body">
-                        <h3 class="banner_subtitle">CAROL HAS THE BUYERS!</h3>
+                    <div class="sec_body  <?php echo get_field('banner_subtitle_style');?>">
+                        <h3 class="banner_subtitle"><?php echo get_field('page_header_subtitle'); ?></h3>
                         <div class="banner_subtitle_body">
-                            <h1>Your Home Sold Guaranteed or Carol Will Buy It</h1>
-                            <p>Fill out the form below to get your home price now.</p>                              
-                            <div class="banner_form_wrap">                         
-                                <?= do_shortcode('[gravityform id="3" title="false" description="false" ajax="false"]'); ?>
+                            <h1><?php echo get_field('page_header_title'); ?></h1>
+                            <p> <?php echo get_field('page_header_body'); ?></p>                                  
+                            <div class="banner_form_wrap">                                            
+								<?php 
+								$form_shortcode = get_field('form_shortcode');
+								echo do_shortcode($form_shortcode); 
+								?>
                             </div>         
                         </div>
-                        <div class="banner_checklist">
-                            <div class="text_wrap media_flex">
-                                <div class="media_left"><figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/check-icon.png" alt=""></figure></div>
-                                <div class="media_body">
-                                    <h3>Your Home Sold Guaranteed or Carol Will Buy It*</h3>
-                                </div>
-                            </div>
-                            <div class="text_wrap media_flex">
-                                <div class="media_left"><figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/check-icon.png" alt=""></figure></div>
-                                <div class="media_body">
-                                    <h3>100% of your asking Price Guaranteed</h3>
-                                </div>
-                            </div>     
-                            <div class="text_wrap media_flex">
-                                <div class="media_left"><figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/check-icon.png" alt=""></figure></div>
-                                <div class="media_body">
-                                    <h3>Client Satisfaction Guaranteed</h3>
-                                </div>
-                            </div>
-                            <div class="text_wrap media_flex">
-                                <div class="media_left"><figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/check-icon.png" alt=""></figure></div>
-                                <div class="media_body">
-                                    <h3>Moving Truck for Free</h3>
-                                </div>
-                            </div>
-                        </div>    
+                        
+						<?php 
+						$show_checklists = get_field('show_checklists');
+						
+						if ($show_checklists): ?>                                               
+							<?= do_shortcode('[banner_checklist]'); ?>
+						<?php endif; ?>  
+						  
                     </div>
                 </div>
             </div> 
