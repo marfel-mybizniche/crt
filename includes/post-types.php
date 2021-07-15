7e276fd9db8f06330355765061ecd016cd94f329
riz-mbn <rizlee@mybizniche.com>
2021-07-15 09:50:50 +0800

    post types manual

--- includes/post-types.php
+++ includes/post-types.php
@@ -115,3 +115,276 @@ function cptui_register_my_taxes_listings_cat() {
 	register_taxonomy( "listings_cat", [ "listings" ], $args );
 }
 add_action( 'init', 'cptui_register_my_taxes_listings_cat' );
+
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: Testimonials.
+	 */
+
+	$labels = [
+		"name" => __( "Testimonials", "custom-post-type-ui" ),
+		"singular_name" => __( "Testimonial", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Testimonials", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "client_testimonials", "with_front" => true ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-format-chat",
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "client_testimonials", $args );
+
+	/**
+	 * Post Type: Offices.
+	 */
+
+	$labels = [
+		"name" => __( "Offices", "custom-post-type-ui" ),
+		"singular_name" => __( "Office", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Offices", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "offices", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "offices", $args );
+
+	/**
+	 * Post Type: Videos.
+	 */
+
+	$labels = [
+		"name" => __( "Videos", "custom-post-type-ui" ),
+		"singular_name" => __( "Video", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Videos", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => false,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "videos", "with_front" => true ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-format-video",
+		"supports" => [ "title", "thumbnail" ],
+		"taxonomies" => [ "category" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "videos", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
+
+function cptui_register_my_cpts_client_testimonials() {
+
+	/**
+	 * Post Type: Testimonials.
+	 */
+
+	$labels = [
+		"name" => __( "Testimonials", "custom-post-type-ui" ),
+		"singular_name" => __( "Testimonial", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Testimonials", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "client_testimonials", "with_front" => true ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-format-chat",
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "client_testimonials", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_client_testimonials' );
+
+
+function cptui_register_my_cpts_offices() {
+
+	/**
+	 * Post Type: Offices.
+	 */
+
+	$labels = [
+		"name" => __( "Offices", "custom-post-type-ui" ),
+		"singular_name" => __( "Office", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Offices", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "offices", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "offices", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_offices' );
+
+
+function cptui_register_my_cpts_videos() {
+
+	/**
+	 * Post Type: Videos.
+	 */
+
+	$labels = [
+		"name" => __( "Videos", "custom-post-type-ui" ),
+		"singular_name" => __( "Video", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Videos", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => false,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "videos", "with_front" => true ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-format-video",
+		"supports" => [ "title", "thumbnail" ],
+		"taxonomies" => [ "category" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "videos", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_videos' );
+
+
+function cptui_register_my_taxes_video_cat() {
+
+	/**
+	 * Taxonomy: Videos Category.
+	 */
+
+	$labels = [
+		"name" => __( "Category", "custom-post-type-ui" ),
+		"singular_name" => __( "Categories", "custom-post-type-ui" ),
+	];
+
+	
+	$args = [
+		"label" => __( "Category", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'video_cat', 'with_front' => true, ],
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"rest_base" => "video_cat",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => true,
+		"show_in_graphql" => false,
+	];
+	register_taxonomy( "video_cat", [ "videos" ], $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_video_cat' );
