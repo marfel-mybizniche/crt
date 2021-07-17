ff664629a9e06695a3490bd4e099ccd83f299df9
Rizlee Lora <rizlee@mybizniche.com>
2021-07-17 14:17:13 +0800

    update single page listings

--- single-listings.php
+++ single-listings.php
@@ -1,153 +1,20 @@
-<?php 
-    /* Template Name: About template */
+<?php
+ /**
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
     get_header();
 ?>
-<section class="hero hero-s4">
-    <div class="hero-block">
-        <figure class="bg"><img src="<?php echo MBN_ASSETS_URI ?>/img/img_hero-bg.jpg" alt=""></figure>
-        <div class="grid-container"> 
-            <div class="grid-x hero-caption">
-                <div class="cell large-7 hero-caption-desktop">
-                    <h3 class="banner_subtitle">CAROL HAS THE BUYERS!</h3>
-                    <h1>Your Home Sold Guaranteed or Carol Will Buy It</h1>
-                    <p>Fill out the form below to get your home price now.</p>                              
-                    <div class="banner_form_wrap">                         
-                        <?= do_shortcode('[gravityform id="3" title="false" description="false" ajax="false"]'); ?>
-                    </div>         
-                
-                    <div class="divider_double_line"></div>  
-                                          
-                    <div class="banner_checklist">
-                        <div class="text_wrap media_flex">
-                            <div class="media_left"><figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/check-icon.png" alt=""></figure></div>
-                            <div class="media_body">
-                                <h3>Your Home Sold Guaranteed or Carol Will Buy It*</h3>
-                            </div>
-                        </div>
-                        <div class="text_wrap media_flex">
-                            <div class="media_left"><figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/check-icon.png" alt=""></figure></div>
-                            <div class="media_body">
-                                <h3>100% of your asking Price Guaranteed</h3>
-                            </div>
-                        </div>     
-                        <div class="text_wrap media_flex">
-                            <div class="media_left"><figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/check-icon.png" alt=""></figure></div>
-                            <div class="media_body">
-                                <h3>Client Satisfaction Guaranteed</h3>
-                            </div>
-                        </div>
-                        <div class="text_wrap media_flex">
-                            <div class="media_left"><figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/check-icon.png" alt=""></figure></div>
-                            <div class="media_body">
-                                <h3>Moving Truck for Free</h3>
-                            </div>
-                        </div>
-                    </div>    
-                </div>
-                <div class="cell small-10 small-offset-1 large-5 large-offset-0 align-self-bottom">
-                    <figure class="right_img text-center"><img src="<?php echo MBN_ASSETS_URI ?>/img/img_vikki-carol-tim.png" alt=""></figure>                        
-                    <div class="text_wrap text-center right_text">
-                        <h2 class="ct_margin_both_0">Carol Royse</h2>
-                        <p class="small">REAL ESTATE EXPERT & RADIO HOST <Br/>OF THE CAROL ROYSE REAL ESTATE RADIO SHOW</p>
-                    </div>                      
-                    <div class="text_wrap row_column_flex text-center">
-                        <div class="row_col row_col_3">
-                            <h2 class="ct_margin_both_0">Vikki</h2>
-                            <h3>Royse-Middlebrook</h3>
-                        </div>
-                        <div class="row_col_3 row_column_flex">                                
-                            <div class="row_col_2">
-                                <figure class="text-center"><img src="<?php echo MBN_ASSETS_URI ?>/img/logo-ktar.png" alt=""></figure>
-                                <span>
-                                    <p class="ct_twoline_text tiny">Saturday</p>
-                                    <p class="ct_twoline_text tiny">2:00 to 3:00pm</p>
-                                </span>
-                            </div>                                
-                            <div class="row_col row_col_2">
-                                <figure class="text-center"><img src="<?php echo MBN_ASSETS_URI ?>/img/logo-550kfyi.png" alt=""></figure>
-                                <span>
-                                    <p class="ct_twoline_text tiny">Sunday</p>
-                                    <p class="ct_twoline_text tiny">2:00 to 3:00pm</p>
-                                </span>
-                            </div>
-                        </div>
-                        <div class="row_col row_col_3">
-                            <h2 class="ct_margin_both_0 ct_twoline_text">Tim</h2>
-                            <h2 class="ct_margin_both_0 ct_twoline_text">Evans</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+<section class="hero_banner_wrap ">
+	<div class="hero_banner">
+		<figure class="bg">
+			<?php echo get_the_post_thumbnail(get_the_ID(),'large'); ?>
+		</figure>
     </div>
 </section>
-<section class="sec_column_item column_template_2 about_template">
-    <div class="grid-container">
-        <div class="grid-x grid-margin-x cols2-s2 ">
-            <div class="cell medium-12 large-6 column_left">
-                <div class="sec_body">
-                    <div class="text_header">
-                        <div class="sec_title">
-                            <div class="subtitle">Learn Why We Love What We Do</div>
-                            <h2 class="title">About The Carol Royse Team</h2>
-                            <p>“There is a higher calling to serving people.”</p>
-                        </div>
-                        <p>In 1985, Carol was a PTA President, a mother of two. Following the death of her husband, she needed a source of income to provide for her family. Her friend suggested she become a realtor, and she enrolled in real estate school. A month later, she was licensed and ready.</p>
-                        <p>Fast forward 10 years. Carol had become an established realtor and was joined by her daughter, Vikki Royse Middlebrook, and son-in-law, Eric Middlebrook. She hired a few administrative assistants, and The Carol Royse Team was born!</p>
-                        <p>In 2001, Carol’s son, Tim Evans, joined the team, thus making it a true “family business”. The Carol Royse Team thrived and expanded over the years, through great markets, and a Great Recession. Today there are 25 members on The Carol Royse Team, and we’ve helped serve over 7,000 families. Our team’s mission, “To Elevate Others to Prosper”, has been a leading cause to our success.</p>
-                        <p>The Carol Royse Team has become one of the top teams across the United States of America. The team currently has 8 family members, all licensed and serving the team in various departments, along with 17 additional agents and admin. Each day, The Carol Royse Team strives to elevate our clients with an exceptional experience. Exceeding their expectations is our team’s daily commitment.</p>
-                        <?= do_shortcode('[video mp4=' . "https://www.youtube.com/watch?v=eRkvJ71O2yc" . ' poster = "https://img.youtube.com/vi/eRkvJ71O2yc/default.jpg"]'); ?>
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="cell medium-12 large-6 col-item column_right">
-                <div class="sec_body">
-                    <div class="text_wrap media_flex">
-                        <div class="media_body">
-                            <figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/bullseye.png" alt=""></figure>
-                            <h3>Our Mission</h3>
-                            <p>Elevate Others To Prosper Clients <br/>Team, Business Partners and Community</p>
-                        </div>
-                    </div>                
-                    <div class="text_wrap  media_flex">
-                        <div class="media_body">
-                            <figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/heart-hands.png" alt=""></figure>
-                            <h3>Our Core Values</h3>
-                            <ul>
-                                <li>Elevate Our Clients to Have an Exceptional</li>
-                                <li>Elevate Our Team Personally and Professionally</li>
-                                <li>Elevate Our Business Partners to Enjoy More</li>
-                                <li>Elevate Our Community By Creating Awareness <br/> and Sharing Resources
-                            </ul>
-                        </div>
-                    </div>  
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<section class="sec_info_wrap">    
-    <div class="grid-container">
-        <div class="grid-x grid-margin-x cols2-s2 ">
-            <div class="cell small-6 medium-6 large-6 col-item">
-                <a class="info_box" href="tel:4807765231">
-                    <figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/phone.png" alt=""></figure>
-                    <div class="address">Metro</div>
-                    <div class="address">Phoenix</div>
-                    <div class="phone">480-776-5231</div>
-                </a>
-            </div>  
-            <div class="cell small-6 medium-6 large-6 col-item">
-                <a class="info_box" href="tel:9282273336">
-                    <figure class="icon_wrapper"><img src="<?php echo MBN_ASSETS_URI ?>/img/phone.png" alt=""></figure>
-                    <div class="address">Northern</div>
-                    <div class="address">Arizona</div>
-                    <div class="phone">928-227-3336</div>
-                </a>
-            </div>
-        </div>
-    </div>
-</section>
+
 <?php 
     get_footer(); 
