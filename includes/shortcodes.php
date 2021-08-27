d636fbf90888bddaa2d9c10d0e7b582f58051927
Mar <marfel@mybizniche.com>
2021-08-27 21:23:33 +0800

    podcast css

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -819,6 +819,8 @@ add_shortcode( 'mbn_sold_listings', 'mbn_sold_listings' );
 function mbn_podcast_news_box($atts) {
 	$podcast_news_box = "";
 
+    $podcast_news_box = "<div class='podcast_news_app'>";
+    
 	if( have_rows('podcast_programs') ):
 
     $podcast_news_box .= "<ul class='podcast_news_items'>";
@@ -860,7 +862,7 @@ function mbn_podcast_news_box($atts) {
 
     endif;
 
-
+    $podcast_news_box = "</div>";
 
 
 	return $podcast_news_box;
