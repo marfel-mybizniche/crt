36b743294500db73727682e850b1ba7a5ff6b02c
riz-mbn <rizlee@mybizniche.com>
2021-09-09 23:40:59 +0800

    taxonomy supplimental custom

--- includes/public-hooks.php
+++ includes/public-hooks.php
@@ -35,4 +35,4 @@ function wpse_modify_taxonomy() {
     register_taxonomy( 'supplimental', 'listing', (array) $category_args );
 }
 // hook it up to 11 so that it overrides the original register_taxonomy function
-add_action( 'init', 'wpse_modify_taxonomy', 11 );
+//add_action( 'init', 'wpse_modify_taxonomy', 11 );
