1e7c93fdb84435654969b295656ec02f8e7272da
Mar <marfel@mybizniche.com>
2021-09-20 22:15:02 +0800

    blog

--- category.php
+++ category.php
@@ -2,49 +2,6 @@
 <section class="blog_content">
         <div class="grid-container">
 
-            <?php if(is_home()) { ?>
-            <div class="whats_new">
-                <h5>What’s New</h5>
-                <?php 
-                    query_posts('posts_per_page=1');
-                
-                    while ( have_posts() ) : the_post(); 
-                        $terms = get_the_terms( $post->ID, 'category' ); 
-                    ?>
-                        
-                    <div class="grid-x grid_item">
-                        <div class="cell large-6">
-                            <figure class="wp-block-image media"><a href="<?php the_permalink(); ?>">
-                                <?php
-                                if ( has_post_thumbnail() ) {
-                                    the_post_thumbnail(); 
-                                } else {
-                                    echo '<img loading="lazy" src="'.home_url().'/wp-content/uploads/2021/07/img-6-steps.jpg" width="450" height="250">';
-                                } ?>
-                            </a></figure>
-                        </div>
-                        <div class="cell large-5 large-offset-1 align-self-middle">
-                            <div class="copy">
-                            <h6>
-                                <?php if($terms) foreach( $terms as $term ) {
-                                    echo '<a href="'.get_category_link( $term->term_id ).'">'.$term->name.'</a>';
-                                } ?>
-                            </h6>
-                            <h3>
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php the_title() ?>
-                                </a>
-                            </h3>
-                            <?php the_excerpt(); ?>
-                            <a class="read-more" href="<?php the_permalink(); ?>">Read More <span>&#129122;</span></a>
-                            </div>
-                        </div>
-                    </div>  
-
-                    <?php endwhile; wp_reset_query();  ?>
-            </div>
-            <?php } ?>
-
             <div class="blog_cats">
                 <?php  
                     $obj_id = get_queried_object_id(); 
@@ -67,9 +24,6 @@
 
             <?php echo '<div class="grid-x grid-margin-x blog_lists">';
                 
-                query_posts('offset=1');
-                query_posts('posts_per_page=-1');
-
                 while ( have_posts() ) : the_post(); 
                     $terms = get_the_terms( $post->ID, 'category' ); 
                 ?>
