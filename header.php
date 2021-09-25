fd554771a7d21e0c0bf69914c09618fe5cb41978
Mar <marfel@mybizniche.com>
2021-09-25 23:07:10 +0800

    Update header.php

--- header.php
+++ header.php
@@ -2,11 +2,13 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php echo bloginfo('charset') ?>">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <link rel="icon" type="image/png" href="<?php echo MBN_ASSETS_URI ?>/img/favicon.png"> -->
-    <!-- <title><?php bloginfo('title') ?></title> -->
+    <?php /*
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <link rel="icon" type="image/png" href="<?php echo MBN_ASSETS_URI ?>/img/favicon.png">
+    */ ?>
+    
 
     <?php wp_head() ?>
 
