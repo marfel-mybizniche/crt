b7803d85091f9c38ffb197aee17c36794b5adac9
Mar <marfel@mybizniche.com>
2021-10-21 14:40:28 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -122,7 +122,8 @@ function mbn_testimonials_shortcode(){
                     $returnhtml .= '<div class="testimonial_blockitem">';
                     $returnhtml .= ($testimonial_img) ? '<figure class="col-image" style="height:242px"><img src="'. $testimonial_img .'" alt=""></figure>' : '<figure><img src="https://via.placeholder.com/1200x500"/></figure>';
                     $returnhtml .= '<div class="testimonial_body">';    
-                    $returnhtml .= '<span data-video-id="'.esc_attr($video_id) .'" class="modal-toggle testimonial_vbtn" data-video-type="'.esc_attr($video_type).'">';
+                    //$returnhtml .= '<span data-video-id="'.esc_attr($video_id) .'" class="modal-toggle testimonial_vbtn" data-video-type="'.esc_attr($video_type).'">';
+                    $returnhtml .= '<span data-src="'.$video_url.'" class="testimonial_vbtn" data-fancybox>';
                     $returnhtml .= '<figure><img src="'. MBN_ASSETS_URI .'/img/icn-play-w.svg" alt=""></figure><span>PLAY VIDEO</span></span>';
                     $returnhtml .= '<div class="testimonial_info">'. $testimonial_rating;
                     $returnhtml .= ( $short_excerpt ) ? '<h3>'. $short_excerpt .'</h3>': '';
